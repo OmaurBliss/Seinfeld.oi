93642aabbd97c5fec2d4541197969f93711e712a
Betiel <betyuser15@gmail.com>
2021-02-02 14:19:22 -0600

    added api error handling

--- Assets/script.js
+++ Assets/script.js
@@ -6,77 +6,86 @@
 
 
 //global variables for local storage and session storage
-var userLibrary = JSON.parse(localStorage.getItem('myBooks'))|| [];
+var userLibrary = JSON.parse(localStorage.getItem('myBooks')) || [];
 var storedMovieSearches = []
 
-function callOMDB(movie){
+function callOMDB(movie) {
   var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(data) {
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    success: function (data) {
       var Title = data.Title;
-        //call render movie function
-        renderMovie(data);
-        storeSearch (Title);
-    });
-  };
- 
+      //call render movie function
+      renderMovie(data);
+      storeSearch(Title);
+    },
+    error: handleAPICallError,
+  });
+};
+
 
 //calls GoogleBooks API
-function bookSearch(Title){
-    var search = Title 
+function bookSearch(Title) {
+  var search = Title
 
-  var queryURL = "https://www.googleapis.com/books/v1/volumes?q="+search;
-  
-        $.ajax({
-          url: queryURL,
-          dataType: "json",
-  
-          success: function(response){
-            renderBooks(response);
-          },
-          type: "GET"
-        });
-        
-  };
+  var queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + search;
 
-  //render info from OMDB API to modal
-  function renderMovie(data){
-     
-      $(".modal-body").empty();
-      var movieTitle = data.Title;
-      var moviePosterURL = data.Poster;
-      var genre = data.Genre;
-      var releaseDate = data.Released;
-      var plot = data.Plot;
-      var titleEl = $("h5");
-      var genreEl = $("<p>");
-      var plotEl = $("<p>");
-      var imgEl = $("<img>");
-      var releasedEl = $("<p>")
+  $.ajax({
+    url: queryURL,
+    dataType: "json",
+    type: "GET",
+    success: function (response) {
+      renderBooks(response);
+    },
+    error: handleAPICallError,
+  });
 
-      titleEl.text(movieTitle);
-      genreEl.text(genre);
-      plotEl.text(plot)
-      imgEl.attr({"src": moviePosterURL, "alt": "movie poster"})
-      releasedEl.text("Released: "+releaseDate);
+};
 
-      $("#findBooks").attr("data-movie", movieTitle);
-      $(".modal-body").append(genreEl, imgEl, plotEl, releasedEl)
-    $("#movieModal").modal("show");
-  };
-  
-  //render info from Google Books API to results container
-  function renderBooks(response){
-    
-    $(".results").empty();
-    for(var i = 0; i < 4; i++){
-      var imageLink = response.items[i].volumeInfo.imageLinks.thumbnail;
-      var bookTitle = response.items[i].volumeInfo.title;
-      var author = response.items[i].volumeInfo.authors;
-      var bookSummary = response.items[i].volumeInfo.description;
-      var bookHtml = `<section style="margin-bottom: 40px; padding: 30px; background-color:  rgb(128, 0, 0);" class="container">
+function handleAPICallError(xhr) {
+  alert("Error has occurred : " + "\r\n\tStatus code: " + xhr.status + "\r\n\tError Message: " + xhr.responseJSON.Error);
+}
+
+
+
+
+//render info from OMDB API to modal
+function renderMovie(data) {
+
+  $(".modal-body").empty();
+  var movieTitle = data.Title;
+  var moviePosterURL = data.Poster;
+  var genre = data.Genre;
+  var releaseDate = data.Released;
+  var plot = data.Plot;
+  var titleEl = $("h5");
+  var genreEl = $("<p>");
+  var plotEl = $("<p>");
+  var imgEl = $("<img>");
+  var releasedEl = $("<p>")
+
+  titleEl.text(movieTitle);
+  genreEl.text(genre);
+  plotEl.text(plot)
+  imgEl.attr({ "src": moviePosterURL, "alt": "movie poster" })
+  releasedEl.text("Released: " + releaseDate);
+
+  $("#findBooks").attr("data-movie", movieTitle);
+  $(".modal-body").append(genreEl, imgEl, plotEl, releasedEl)
+  $("#movieModal").modal("show");
+};
+
+//render info from Google Books API to results container
+function renderBooks(response) {
+
+  $(".results").empty();
+  for (var i = 0; i < 4; i++) {
+    var imageLink = response.items[i].volumeInfo.imageLinks.thumbnail;
+    var bookTitle = response.items[i].volumeInfo.title;
+    var author = response.items[i].volumeInfo.authors;
+    var bookSummary = response.items[i].volumeInfo.description;
+    var bookHtml = `<section style="margin-bottom: 40px; padding: 30px; background-color:  rgb(128, 0, 0);" class="container">
       <div class="container ">
       <div class="card-group vgr-cards">
       <div  class="card">
@@ -91,42 +100,42 @@ function bookSearch(Title){
       </div>
       </div>
       </section>`;
-      
-           
-      $(".results").append(bookHtml);
+
+
+    $(".results").append(bookHtml);
+  }
+
+  //setting to and retrieving from local storage
+  $(".btn-book").on("click", function (e) {
+    e.preventDefault
+    var savedBook = {
+      "Title": $(this).attr("data-book"),
+      "Author": $(this).siblings(".author").html(),
+      "Summary": $(this).siblings(".summary").html(),
+      "Cover": $(this).parent().siblings(".card-img-body").children(".card-img").attr("src")
     }
 
-    //setting to and retrieving from local storage
-    $(".btn-book").on("click", function(e){
-      e.preventDefault
-      var savedBook = {
-        "Title": $(this).attr("data-book"),
-        "Author": $(this).siblings(".author").html(),
-        "Summary": $(this).siblings(".summary").html(),
-        "Cover": $(this).parent().siblings(".card-img-body").children(".card-img").attr("src")
-      }
+    userLibrary.unshift(savedBook);
+    storeBooks();
+  });
 
-      userLibrary.unshift(savedBook);
-      storeBooks();
-    });
+}
 
-  }
+function storeBooks() {
+  localStorage.setItem("myBooks", JSON.stringify(userLibrary));
+}
 
-    function storeBooks(){
-    localStorage.setItem("myBooks", JSON.stringify(userLibrary));
-  }
 
-  
-  //called when libraryindex.html loads
-  function loadMyLibrary(){
+//called when libraryindex.html loads
+function loadMyLibrary() {
 
-      for(var j = 0; j < userLibrary.length; j++){
-        var savedTitle = userLibrary[j].Title;
-        var savedAuthor = userLibrary[j].Author;
-        var savedSummary = userLibrary[j].Summary;
-        var savedCover = userLibrary[j].Cover;
+  for (var j = 0; j < userLibrary.length; j++) {
+    var savedTitle = userLibrary[j].Title;
+    var savedAuthor = userLibrary[j].Author;
+    var savedSummary = userLibrary[j].Summary;
+    var savedCover = userLibrary[j].Cover;
 
-        var savedHtml = `<section style="margin-bottom: 40px; padding: 30px; background-color:  rgb(128, 0, 0);" class="container">
+    var savedHtml = `<section style="margin-bottom: 40px; padding: 30px; background-color:  rgb(128, 0, 0);" class="container">
         <div class="container">
         <div class="card-group vgr-cards">
         <div class="card">
@@ -142,47 +151,46 @@ function bookSearch(Title){
         </div>
         </section>`;
 
-        $(".myLibrary").append(savedHtml);
-      }
-    
-  };
-  
+    $(".myLibrary").append(savedHtml);
+  }
 
-  //click-event handlers
-  //initialize search
-  $("#firstSearch").on("click", function(e){
-    e.preventDefault;
-     var search = $("#books").val().trim();
-    if(search){
-      callOMDB(search);
-    }else{
-      return;
-    };
-    $("#books").val("");
-    // storeSearch (search);
-  });
-  
-  function storeSearch (search) {
-    storedMovieSearches.unshift(search)
-    sessionStorage.setItem("recent",JSON.stringify (storedMovieSearches))
- console.log(sessionStorage.getItem("recent"))  
+};
+
+
+//click-event handlers
+//initialize search
+$("#firstSearch").on("click", function (e) {
+  e.preventDefault;
+  var search = $("#books").val().trim();
+  if (search) {
+    callOMDB(search);
+  } else {
+    return;
+  };
+  $("#books").val("");
+  // storeSearch (search);
+});
+
+function storeSearch(search) {
+  storedMovieSearches.unshift(search)
+  sessionStorage.setItem("recent", JSON.stringify(storedMovieSearches))
+  console.log(sessionStorage.getItem("recent"))
 }
 
 
 //modal click events
- $("#findBooks").on("click", function(){
+$("#findBooks").on("click", function () {
   bookSearch($(this).attr("data-movie"));
   $("#movieModal").modal("hide");
- });
+});
 
-  //close modal
-  $(".closeModal").on("click", function(){
-    $("#firstSearch").val("");
-    $("#movieModal").modal("hide");
-  });
+//close modal
+$(".closeModal").on("click", function () {
+  $("#firstSearch").val("");
+  $("#movieModal").modal("hide");
+});
 
-  //clear search results field
-$("#clear-search").on("click", function(){
+//clear search results field
+$("#clear-search").on("click", function () {
   $(".results").empty();
 })
-  
